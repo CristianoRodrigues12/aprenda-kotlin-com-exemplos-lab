c6edbb4b0d8723822512fb7a07aa758055c2f2cd
Cristiano Rodrigues <100481631+CristianoRodrigues12@users.noreply.github.com>
2023-05-17 14:12:47 -0300

    Usuarios, Matricula e notas

--- desafio.kt
+++ desafio.kt
@@ -2,20 +2,76 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(val nome: String, val email: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 180)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val nivel: Nivel, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        if (inscritos.contains(usuario)) {
+            println("${usuario.nome} já está matriculado nesta formação.")
+        } else {
+            inscritos.add(usuario)
+            println("${usuario.nome} foi matriculado na formação $nome.")
+        }
+    }
+
+    fun registrarNota(usuario: Usuario, conteudo: ConteudoEducacional, nota: Int) {
+        if (inscritos.contains(usuario)) {
+            println("Nota $nota registrada para ${usuario.nome} no conteúdo ${conteudo.nome}.")
+        } else {
+            println("${usuario.nome} não está matriculado nesta formação.")
+        }
+    }
+
+    fun listarInscritos() {
+        if (inscritos.isNotEmpty()) {
+            println("Inscritos na formação $nome:")
+            inscritos.forEach { println("- ${it.nome}") }
+        } else {
+            println("Não há inscritos nesta formação.")
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuarios = listOf(
+        Usuario("Alice", "alice@gmail.com"),
+        Usuario("Bob", "bob@hotmail.com"),
+        Usuario("Carol", "carol@yahoo.com"),
+        Usuario("David", "david@outlook.com"),
+        Usuario("Eve", "eve@aol.com")
+    )
+
+    val conteudos = listOf(
+        ConteudoEducacional("Introdução ao Kotlin", 120),
+        ConteudoEducacional("Fundamentos de programação", 90),
+        ConteudoEducacional("Desenvolvimento mobile", 180),
+        ConteudoEducacional("Arquitetura de software", 150),
+        ConteudoEducacional("Inteligência artificial", 240)
+    )
+
+    val formacao1 = Formacao("Desenvolvimento Android", Nivel.INTERMEDIARIO, listOf(conteudos[0], conteudos[2], conteudos[3]))
+    val formacao2 = Formacao("Programação básica", Nivel.BASICO, listOf(conteudos[1], conteudos[3]))
+    val formacao3 = Formacao("Inteligência artificial avançada", Nivel.DIFICIL, listOf(conteudos[4]))
+
+    formacao1.listarInscritos()
+    formacao1.matricular(usuarios[0])
+    formacao1.matricular(usuarios[0])
+    formacao1.matricular(usuarios[1])
+    formacao1.listarInscritos()
+    formacao1.registrarNota(usuarios[0], conteudos[0], 8)
+    formacao1.registrarNota(usuarios[1], conteudos[1], 7)
+
+    formacao2.listarInscritos()
+    formacao2.matricular(usuarios[2])
+    formacao3.matricular(usuarios[3])
+    formacao3.matricular(usuarios[4])
+    formacao3.listarInscritos()
+    formacao2.registrarNota(usuarios[2], conteudos[1], 9)
+    formacao3.registrarNota(usuarios[3], conteudos[4], 10)
+    formacao3.registrarNota(usuarios[4], conteudos[4], 9) 
 }
